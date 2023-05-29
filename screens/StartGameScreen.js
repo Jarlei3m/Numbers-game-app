48b92ffd31ed8812dbf1664d65aac8f06e52d79c
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2023-05-29 20:02:44 -0300

    style: Start game screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,9 +4,21 @@ import PrimaryButton from "../components/PrimaryButton";
 function StarGameScreen() {
   return (
     <View style={styles.startGameScreenContainer}>
-      <TextInput />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
@@ -18,12 +30,30 @@ const styles = StyleSheet.create({
     padding: 16,
     marginTop: 100,
     marginHorizontal: 24,
-    backgroundColor: "#72063c",
+    backgroundColor: "#3b021f",
     borderRadius: 8,
     elevation: 4, // works only for Android
     shadowColor: "black", // works only for IOS
     shadowOffset: { width: 0, height: 2 }, // works only for IOS
     shadowRadius: 6, // works only for IOS
-    shadowOpacity: 0.25,
+    shadowOpacity: 0.25, // works only for IOS
+    alignItems: "center",
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    fontWeight: "bold",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
